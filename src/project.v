e280d82fe0c20d9d475d5496936b5105e40cc7e8
abishekmarimuthu2005 <abishekmarimuthu2005@gmail.com>
2026-06-02 15:34:01 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,28 +1,30 @@
 `default_nettype none
 
-module tt_um_axis_rx (
+module tt_um_axi_rx (
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
     output wire [7:0] uio_out,
     output wire [7:0] uio_oe,
-    input  wire clk,
-    input  wire rst_n
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-    reg [7:0] data;
+    reg [7:0] data_reg;
 
-    wire tvalid = uio_in[0];
-    assign uio_out[0] = 1'b1;   // tready always high
-    assign uio_oe = 8'b00000001;
-
-    always @(posedge clk) begin
+    always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
-            data <= 8'h00;
-        else if (tvalid)
-            data <= ui_in;
+            data_reg <= 8'h00;
+        else
+            data_reg <= ui_in;
     end
 
-    assign uo_out = data;
+    assign uo_out  = data_reg;
+
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
+
+    wire _unused = &{ena, uio_in, 1'b0};
 
 endmodule
